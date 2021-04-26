a8f813fe1d4790489a3ee0476f20e735daf1ebd9
Dan Goscomb <dan.goscomb@hso.co.uk>
2021-04-26 18:09:29 +0100

    initial release

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github')
+const fs = require('fs');
+const xml2js = require('xml2js');
 
-
-// most @actions toolkit packages have async methods
-async function run() {
+function parseIt(file) {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    var parser = new xml2js.Parser();
+    var data = fs.readFileSync(file);
+    let parsed = parser.parseString(data);
+    if (!parsed) {
+      throw{message: 'File is not XML'}
+    }
+    let newMessage = '### :white_check_mark: Result of Pytest Coverage\n'
+    newMessage += '| Name | Cover |\n| :--- | ----: |\n'
+    parser.resultObject.coverage.packages[0].package.forEach((p) => {
+      p.classes[0].class.forEach((c) => {
+        if(c['$']['line-rate'] < 1) {
+          newMessage += `| ${c['$'].filename} | ${Math.round(parseFloat(c['$']['line-rate']) * 100)} |\n`;
+        }
+      });
+    });
+    return newMessage;
   } catch (error) {
+    throw error
+  }
+}
+
+async function run() {
+  if (github.context.eventName === 'pull_request') {
+    core.info('Comment only will be created on pull requests!')
+    return
+  }
+  try {
+    const file = core.getInput('file');
+    const githubToken = core.getInput('token');
+    if (!fs.existsSync(file)) {
+      throw {message: `File ${file} does not exist`};
+    }
+    let msg = parseIt(file);
+    const context = github.context
+    const pullRequestNumber = context.payload.pull_request?.number
+
+    const octokit = github.getOctokit(githubToken)
+
+    // Now decide if we should issue a new comment or edit an old one
+    const {data: comments} = await octokit.issues.listComments({
+      ...context.repo,
+      issue_number: pullRequestNumber ?? 0
+    })
+
+    const comment = comments.find((comment) => {
+      return (
+        comment.user.login === 'github-actions[bot]' &&
+        comment.body.startsWith(
+          '### :white_check_mark: Result of Pytest Coverage\n'
+        )
+      )
+    })
+
+    if (comment) {
+      await octokit.issues.updateComment({
+        ...context.repo,
+        comment_id: comment.id,
+        body: msg
+      })
+    } else {
+      await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pullRequestNumber ?? 0,
+        body: msg
+      })
+    }
+  }
+  catch (error) {
     core.setFailed(error.message);
   }
 }
